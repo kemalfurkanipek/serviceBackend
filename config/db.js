7d69a713f4cf228e908d042cc1c22149fd5f1add
Kemal Furkan İPEK <97844929+kemalfurkanipek@users.noreply.github.com>
2026-04-07 01:08:41 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://kemalfurkanipek:2gNo12ETkGDwnk1i@cluster0.jkxxoc4.mongodb.net/?appName=Cluster0');
+    await mongoose.connect('mongodb+srv://service_db_user:0jOGKqj6dfJPO06N@clusterservice.i5swrvh.mongodb.net/?appName=ClusterService');
     console.log('MongoDB\'ye başarıyla bağlandı');
   } catch (error) {
     console.error('MongoDB bağlantı hatası:', error);
